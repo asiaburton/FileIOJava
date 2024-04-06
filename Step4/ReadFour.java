efc3c67cb1a6f2ecec68da99f177517b0ccfd5f3
Asia <asia@zipcodersmbp18.mynetworksettings.com>
2024-04-06 17:52:32 -0400

    'File not found' exceptions instead of expected output. Review needed

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -10,9 +10,20 @@ public class ReadFour
      * Yes, you can look at the other 
      * examples in this lab to build your solution here.
      */
-        public static void main(String[] args)
-        {
-            // Print out a running total of all the
-            // values in the input file.
+        public static void main(String[] args) {
+            try {
+                Scanner fileIn = new Scanner(new File("input.txt"));
+
+                while (fileIn.hasNext()) {
+                    // Print out a running total of all the
+                    // values in the input file.
+                    String lineIn = fileIn.nextLine();
+                    int sum = 0;
+                    sum += Integer.parseInt(lineIn);
+                    System.out.println(sum);
+                }
+            } catch (IOException e) {
+                System.out.println("File not found");
+            }
         }
 }
